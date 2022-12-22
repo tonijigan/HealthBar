0662cc4f226f634d2342aed6016a2e4cf0d6e1be
tonijigan <100856397+tonijigan@users.noreply.github.com>
2022-12-22 19:29:44 +0300

    init

--- Assets/Scripts/HealthBar.cs
+++ Assets/Scripts/HealthBar.cs
@@ -4,14 +4,48 @@ using UnityEngine.UI;
 
 public class HealthBar : MonoBehaviour
 {
+    [SerializeField] private Slider _slider;
     [SerializeField] private float _changeSliderSpeed;
 
-    public IEnumerator ChangeHealth(Slider health, float target, float direction)
+    private Coroutine _coroutine;
+    private Player _player;
+
+    private void Start()
     {
-        while (health.value != target)
+        _player = GetComponent<Player>();
+        _slider.maxValue = _player.Health;
+        _slider.value = _slider.maxValue;
+    }
+
+    public void TakeSliderValue()
+    {
+        HaveCoroutine();
+        _coroutine = StartCoroutine(ChangeHealth());
+    }
+
+    public void DecreaseSliderValue()
+    {
+        HaveCoroutine();
+        _coroutine = StartCoroutine(ChangeHealth());
+    }
+
+    public IEnumerator ChangeHealth()
+    {
+        float distance = 5;
+
+        while (_slider.value != _player.Health)
         {
-            health.value = Mathf.MoveTowards(health.value, target, direction * Time.deltaTime * _changeSliderSpeed);
+            _slider.value = Mathf.MoveTowards(_slider.value, _player.Health, distance * Time.deltaTime * _changeSliderSpeed);
             yield return null;
         }
+    }
+
+    private Coroutine HaveCoroutine()
+    {
+        if (_coroutine != null)
+        {
+            StopCoroutine(_coroutine);
+        }
+        return _coroutine;
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,49 +1,39 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Events;
 
-[RequireComponent(typeof(HealthBar))]
 public class Player : MonoBehaviour
 {
-    [SerializeField] private Slider _health;
+    [SerializeField] private float _health;
     [SerializeField] private float _addHealth;
     [SerializeField] private float _damage;
+    [SerializeField] private UnityEvent _takeHealth, _takeDamage;
 
-    private HealthBar _healthBar;
-    private Coroutine _coroutine;
-    private float _target;
+    private float _maxHealth;
+
+    public float Health => _health;
+
+    private void Start()
+    {
+        _maxHealth = _health;
+    }
 
     public void TakeHealth()
     {
-        _target = _health.value + DividerDirection(_addHealth);
-        HaveCoroutine();
-        _coroutine = StartCoroutine(_healthBar.ChangeHealth(_health, _target, _addHealth));
+        if (_health < _maxHealth)
+        {
+            _health += _addHealth;
+            _takeHealth.Invoke();
+        }
     }
 
     public void TakeDamage()
     {
-        _target = _health.value - DividerDirection(_damage);
-        HaveCoroutine();
-        _coroutine = StartCoroutine(_healthBar.ChangeHealth(_health, _target, _damage));
-    }
+        float _minHealth = 0;
 
-    private void Start()
-    {
-        _healthBar = GetComponent<HealthBar>();
-        _health.value = _health.maxValue;
-    }
-
-    private float DividerDirection(float direction)
-    {
-        float divider = 100;
-        return direction /= divider;
-    }
-
-    private Coroutine HaveCoroutine()
-    {
-        if (_coroutine != null)
+        if (_health > _minHealth)
         {
-            StopCoroutine(_coroutine);
+            _health -= _damage;
+            _takeDamage.Invoke();
         }
-        return _coroutine;
     }
 }
